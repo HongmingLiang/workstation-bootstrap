4ad0fc819fe1aa681ef4ca72327054af9b20c0a7
HongmingLiang <hm.liang.dev@outlook.com>
2025-11-22 14:22:43 +0800

    detect working env

---
+++ utils/context.py
@@ -0,0 +1,23 @@
+from subprocess import run
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class EnvironmentContext:
+    has_sudo: bool
+    home: Path
+    local_bin: Path
+    
+    @classmethod
+    def detect(cls) -> "EnvironmentContext":
+        has_sudo = (run(['sudo', 'echo', '"detect sudo privileges"'], shell=True ,capture_output=True).returncode == 0)
+        home = Path.home()
+        local_bin = home / '.local' / 'bin'
+        return cls(has_sudo=has_sudo, home=home, local_bin=local_bin)
+    
+    def print_info(self) -> None:
+        print(f"[INFO] Environment Context: has_sudo={self.has_sudo}, home={self.home}, local_bin={self.local_bin}")
+    
+def command_exists(command: str) -> bool:
+    from shutil import which
+    return which(command) is not None
